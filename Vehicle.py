8643892ed54003f504acf413c84cf4294b70128e
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2022-01-26 17:40:24 +0100

    Added wait_time to Vehicle agent

--- Vehicle.py
+++ Vehicle.py
@@ -8,6 +8,9 @@ class Vehicle(Agent):
 	def __init__(self,  model):
 		super().__init__(self, model)
 
-		self.shape = 'bus.png'
+		self.shape = 'clear.png'
 		self.color = ''
+		self.wait_time = 0
 
+	def increase_wait_time(self):
+		self.wait_time += 1
